3083f473964d27452e53eb247cc7680b0eeae9fb
ghost <33994205+stadiumphenom@users.noreply.github.com>
2025-09-25 20:24:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from bs4 import BeautifulSoup
+from beautifulsoup4 import BeautifulSoup
 from scrapling.engines.static import StealthyFetcher
 from scrapling.engines.toolbelt.custom import Response
 
